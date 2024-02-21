2ab874fa9a879654c00fdead72c5ca24afd0c32b
Rosario Evans Pena <rpena@L22JPVKP3Q.local>
2024-02-21 04:56:20 -0600

    source code: train.py first commit

---
+++ ser/train.py
@@ -0,0 +1,44 @@
+import torch
+from torch import optim
+from torch.nn import functional as F 
+from torch.utils.data import DataLoader
+from model import Net
+from data import prepare_data
+
+def train(model, optimizer, training_dataloader, validation_dataloader, epochs, device):
+    """Train the model."""
+    for epoch in range(epochs):
+        for i, (images, labels) in enumerate(training_dataloader):
+            images, labels = images.to(device), labels.to(device)
+            model.train()
+            optimizer.zero_grad()
+            output = model(images)
+            loss = F.nll_loss(output, labels)
+            loss.backward()
+            optimizer.step()
+            print(
+                f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
+                f"| Loss: {loss.item():.4f}"
+            )
+
+        # Validate the model
+        validate_model(model, validation_dataloader, epoch, device)
+
+def validate_model(model, validation_dataloader, epoch, device):
+    """Validate the model."""
+    val_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for images, labels in validation_dataloader:
+            images, labels = images.to(device), labels.to(device)
+            model.eval()
+            output = model(images)
+            val_loss += F.nll_loss(output, labels, reduction="sum").item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(labels.view_as(pred)).sum().item()
+        val_loss /= len(validation_dataloader.dataset)
+        val_acc = correct / len(validation_dataloader.dataset)
+
+        print(
+            f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
+        )
